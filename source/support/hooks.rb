a313af151a756856a64464c7e38f8f3805b14f1d
Juan Diego Sanz Jorquera <juandiego.sanz@intelygenz.com>
2017-11-27 01:08:45 +0100

    Added screenshot

--- source/support/hooks.rb
+++ source/support/hooks.rb
@@ -11,6 +11,16 @@ Before do |scenario|
 end
 
 After do |scenario|
+    if scenario.respond_to?('scenario_outline') then
+        scenario = scenario.scenario_outline
+    end
+    if scenario.failed?
+        last_tag = scenario.tags.last.name
+        scenario_location = scenario.location.to_s.split('/').last
+        file_name = "#{scenario_location}#{last_tag}#{Time.now.strftime('%y-%m-%d_%H:%M')}.png"
+        file_path = File.expand_path(File.dirname(__FILE__) + "/../../report/#{$plat.to_s}/images/" + file_name)
+        @driver.screenshot(file_path)
+    end
     @driver.quit
 end
 
@@ -45,26 +55,12 @@ end
 
 
 =begin    
-    if scenario.respond_to?('scenario_outline') then
-        scenario = scenario.scenario_outline
-    end
-    if scenario.failed?
-        last_tag = scenario.tags.last.name
-        scenario_location = scenario.location.to_s.split('/').last
-        file_name = "#{scenario_location}#{last_tag}#{Time.now.strftime('%y-%m-%d_%H:%M')}.png"
-        file_path = File.expand_path(File.dirname(__FILE__) + "/../../report/#{$plat.to_s}/images/" + file_name)
-        @driver.screenshot(file_path)
-    end
     @driver.driver_quit
 =end
 
 
 =begin
     ## Create global var $date with the actual date
-    <% $date = Time.now.strftime('%y-%m-%d_%H:%M') %>
-    <% dir = "./report/#{$date}" %>
-    <% Dir.mkdir dir%>
-    <% Dir.mkdir "#{dir}/error_images" %>
 
     profile: >
     **
